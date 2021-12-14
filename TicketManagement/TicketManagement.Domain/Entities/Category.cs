e292ba9d23f57304766b4ae620618c7574ccc935
Wendell Arias <wdarias@unicesar.edu.co>
2021-12-14 10:39:32 -0500

    Creación del Dominio

---
+++ TicketManagement/TicketManagement.Domain/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using TicketManagement.Domain.Common;
+
+namespace TicketManagement.Domain.Entities
+{
+    public class Category : AuditableEntity
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Event> Events { get; set; }
+    }
+}
---
+++ TicketManagement/TicketManagement.Domain/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TicketManagement.Domain.Common;
+
+namespace TicketManagement.Domain.Entities
+{
+    public class Order: AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public bool OrderPaid { get; set; }
+    }
+}
